0a2b0f5d82a503db71e5d5e7ace4617e4a4727c4
Matthias Felleisen <matthias@ccs.neu.edu>
2019-08-26 16:47:52 +0200

    add string functionality to exn testing

--- Testing/testing.rkt
+++ Testing/testing.rkt
@@ -14,7 +14,7 @@
  ;; IF recording is set, also record the specified test cases as pairs of files
  r-check-equal?
 
- #; {[-> Void] [Listof JSexpr] String String -> Void}
+ #; {[-> Void] (U String [Listof JSexpr]) String String -> Void}
  ;; convert inputs to JSON, run main on converted inputs, catch exn, compare with expected
  ;; IF recording is set, also record the specified test cases as pairs of files
  r-check-exn)
@@ -28,10 +28,30 @@
 ;; ---------------------------------------------------------------------------------------------------
 (define recording (make-parameter #false))
 
+(define (r-check-equal? main inputs expected msg)
+  (record inputs expected)
+  
+  (define in:str (prepare inputs))
+  (define ex:str (prepare expected))
+
+  (check-equal? (post (with-output-to-bytes (lambda () (with-input-from-bytes in:str main))))
+                expected
+                msg))
+
+(define (r-check-exn main inputs expected msg)
+  (if (string? inputs) 
+      (record (list inputs) (list expected) #:write-inputs displayln)
+      (record inputs (list expected)))
+  
+  (define in:str (prepare inputs))
+  (define rx (pregexp expected))
+  
+  (check-exn rx (λ () (with-output-to-bytes (λ () (with-input-from-bytes in:str main)))) msg))
+
 #;[[Listof Jsexpr] [Listof Jsexpr] -> Void]
 ;; write test input and test output to next pair of test files in (recording) directory, if any 
 (define *file-no 0)
-(define (record input output)
+(define (record input output #:write-inputs (wi send-message))
   (define base (recording))
   (when base
     (unless (directory-exists? base) (make-directory base))
@@ -39,7 +59,7 @@
     (define n (~a *file-no))
     (define -in.json  (build-path base (format "~a-in.json" n)))
     (define -out.json (build-path base (format "~a-out.json" n)))
-    (write-to -in.json input send-message)
+    (write-to -in.json input wi)
     (write-to -out.json output (lambda (x) (send-message x) (newline)))))
 
 ;; [X] [PathString [Listof X] [X -> Void] -> Void]
@@ -47,21 +67,13 @@
 (define (write-to file-name input writer)
   (with-output-to-file file-name (lambda () (for ((x input)) (writer x))) #:exists 'replace))
 
-(define (r-check-equal? main inputs expected msg)
-  (define in:str (prepare inputs))
-  (define ex:str (prepare expected))
-  (record inputs expected)
+#; {(U String [Listof JSexpr]) -> Bytes}
+(define (prepare x)
+  (cond
+    [(string? x) (string->bytes/utf-8 x)]
+    [else (bytes-append (bytes-join (map jsexpr->bytes x) #"\n"))]))
 
-  (check-equal? (post (with-output-to-bytes (lambda () (with-input-from-bytes in:str main))))
-                expected
-                msg))
-
-(define (r-check-exn main inputs expected msg)
-  (define in:str (prepare inputs))
-  (record inputs (list expected))
-  (define rx (pregexp expected))
-  (check-exn rx (λ () (with-output-to-bytes (λ () (with-input-from-bytes in:str main)))) msg))
-  
-(define (prepare x) (bytes-append (bytes-join (map jsexpr->bytes x) #"\n") #"\n"))
-(define (post x) (port->list read-json (open-input-bytes x)))
+#;{Bytes -> [Listof JSexpr]}
+(define (post x)
+  (port->list read-json (open-input-bytes x)))
   
