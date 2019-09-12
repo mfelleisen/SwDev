da839743455e4227eeaa42086612f65eaa3e56a0
Matthias Felleisen <matthias@ccs.neu.edu>
2019-09-12 10:04:36 -0400

    bye bye Github

--- Github/sample-identity.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-(provide personal-token-id)
-
-(require github-api)
-
-(define personal-token "personal token here")
-(define username "username here")
-(define personal-token-id (github-identity 'personal-token (list username personal-token)))
