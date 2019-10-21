7d665c3c1825e69eef8c1f130b3a706e036e54b5
Matthias Felleisen <matthias@ccs.neu.edu>
2019-10-21 09:16:06 -0400

    equip spy with option to render differently

--- Debugging/spy.rkt
+++ Debugging/spy.rkt
@@ -1,20 +1,29 @@
 #lang racket
 
-(provide spy)
+(provide
+ #; { (spy e) || (spy e #:with show) }
+ #; {show : (-> Any Syntax Nat Nat Void)}
+ ;; evaluates e to v
+ ;; EFFECT render v via printing it to the error port or with `show`
+ spy)
 
-(define-syntax spy
-  (lambda (exp)
-    (syntax-case exp ()
-      [(spy e)
-       (with-syntax ([the-expr (syntax e)]
-                     [the-src (syntax-source (syntax e))]
-                     [the-loc (syntax-position #'e)])
-         (syntax
-          (let ([val e])
-            (fprintf (current-error-port)
-                     "Expression~n~a~nat ~a:~a~nevaluated to~n~v~n~n"
-                     (quote the-expr)
-                     (quote the-src)
-                     (quote the-loc)
-                     val)
-            val)))])))
+;; ---------------------------------------------------------------------------------------------------
+(require (for-syntax syntax/parse))
+
+;; ---------------------------------------------------------------------------------------------------
+(define-syntax (spy exp)
+  (syntax-parse exp
+    [(spy e (~optional [~seq #:with rendering] #:defaults ([rendering #'to-error-port])))
+     (with-syntax ([expr #'e]
+                   [src  (syntax-source (syntax e))]
+                   [line (syntax-line #'e)]
+                   [col  (syntax-column #'e)])
+       #'(let ([val e]) (rendering val 'expr src `(line: ,line col: ,col)) val))]))
+
+(define (to-error-port val expr src loc)
+  (fprintf (current-error-port) "Expression~n~a~nat ~a : ~a~nevaluated to~n~v~n~n" expr src loc val))
+
+;; ---------------------------------------------------------------------------------------------------
+(module+ test 
+  (spy (+ 1 1))
+  (spy (+ 1 1) #:with (λ (val expr src loc) (displayln val))))
