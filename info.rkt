69459366f1e580b7efecf6b04f43e88f5422a176
Matthias Felleisen <matthias@ccs.neu.edu>
2020-11-23 22:52:34 -0500

    dependnecies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,6 @@
 #lang info
 (define collection "SwDev")
-(define deps '("base" "gui-lib" "pict-lib" "rackunit-lib"))
-(define build-deps '("gui-lib" "pict-lib" "rackunit-lib" "scribble-lib" "racket-doc" "rackunit-lib"))
+
 (define scribblings '(("scribblings/SwDev.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
@@ -9,3 +8,19 @@
 
 (define compile-omit-paths
   '("Ideas"))
+
+(define deps
+  '("base"
+     "gui-lib"
+     "scribble-lib"
+     "pict-lib"
+     "rackunit-lib"))
+
+(define build-deps
+  '("gui-lib"
+     "pict-lib"
+     "at-exp-lib"
+     "rackunit-lib"
+     "scribble-lib"
+     "racket-doc"
+     "rackunit-lib"))
