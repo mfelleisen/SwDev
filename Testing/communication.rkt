8add5ddd8bffbadb3a111a58273640aaf87a5f02
Matthias Felleisen <matthias@ccs.neu.edu>
2021-11-26 14:00:39 -0500

    undo special-purpos logging

--- Testing/communication.rkt
+++ Testing/communication.rkt
@@ -134,24 +134,13 @@
 ;; EFFECT when the input port is ready, try to read-json & send result on reply-ch
 ;; EFFECT tell control thread on control-ch that the reading has (not) started in `start-timeout`
 (define ((make-reader control-ch reply-ch start-timeout-sec))
-  (when (special) (log-error "waiting for response"))
-  
   (cond
     [(sync/timeout start-timeout-sec (current-input-port))
-
-     (when (special) (log-error "response started"))
-
      (channel-put control-ch 'response-started)
      (with-handlers [(values (lambda (e) (channel-put reply-ch (list 'exn e))))]
        (channel-put reply-ch (list 'ok (read-json))))]
     [else
-
-     (when (special) (log-error "response ____failed___ to start"))
-
      (channel-put control-ch 'response-not-started)]))
-
-(define special (make-parameter #false))
-(provide special)
 
 #; {-> [Channel Channel N -> (U NO-REACTION ERROR-string RESPONSE-INCOMPLETE JSexpr)]}
 ;; retrieve the JSON value after it was read subject to timing constraints 
