ba9908df84d21bfeb56c2fc0097f9518702faef0
Michael Ballantyne <ballantyne.m@northeastern.edu>
2023-10-12 11:15:48 -0400

    refine example executable

--- Testing/TestInfOut/inf2.rkt
+++ Testing/TestInfOut/inf2.rkt
@@ -9,7 +9,7 @@ exec racket -tm "$0" -- ${1+"$@"}
 (require json)
 
 (define (main [count 0])
-  (read-json) ;; may produce EOF, keep going anyway
-  (write-json count) (newline) (flush-output)
+  (define last (read-json)) ;; may produce EOF, keep going anyway
+  (write-json (format "last ~a, count ~a" last count)) (newline) (flush-output)
   (main (add1 count)))
 
